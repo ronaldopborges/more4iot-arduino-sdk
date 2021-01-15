ccbdee4c22214b69c621e38a7e0715c42f1bcdf9
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2021-01-15 11:50:59 -0300

    add: data object json package treatment

--- src/MiddlewareArduinoSdk.h
+++ src/MiddlewareArduinoSdk.h
@@ -1,7 +1,46 @@
 
-#ifndef Middleware_Arduino_h
-#define Middleware_Arduino_h
+#ifndef Middleware_Arduino_Sdk_h
+#define Middleware_Arduino_Sdk_h
 
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "ArduinoJson/Polyfills/type_traits.hpp"
 
+class Middleware
+{
+public:
+  inline Middleware(Client &client){}
+  inline ~Middleware(){}
+
+  bool newDataObject(const char *token, int dataFieldsAmount, double lon = 0.0, double lat = 0.0){
+    jsonRoot.clear();
+    this->fieldsAmount = dataFieldsAmount+3;
+    this->memoryAllocated = JSON_OBJECT_SIZE(this->fieldsAmount);
+    DynamicJsonDocument doc(this->memoryAllocated);
+    jsonRoot = doc.as<JsonObject>();
+    jsonRoot["deviceUuid"]=token;
+    jsonRoot["lat"]=lat;
+    jsonRoot["lon"]=lon;
+    jsonData = jsonRoot.createNestedObject("data");
+    return true;
+  }
+
+  template<typename T>
+  bool addField(const char *nameField, T value){
+    jsonData[nameField]=value;
+    return true;
+  }                                                                                                                                                                                                                                                                                                     
+
+private:
+  inline size_t jsonObjectSize(size_t tam) {return tam*sizeof(ARDUINOJSON_NAMESPACE::VariantSlot);}
+  inline void setFieldsAmount(size_t fieldsAmount){this->fieldsAmount = fieldsAmount;}
+
+  //dataobject
+  JsonObject jsonRoot;
+  JsonObject jsonData;
+  int fieldsAmount;
+  size_t memoryAllocated;
+
+};
 
 #endif
